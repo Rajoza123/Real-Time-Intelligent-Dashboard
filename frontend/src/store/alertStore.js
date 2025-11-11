c267f890f87128d756b37c5e57e8b097fb2dacb2
Rajoza123 <ozaraj76@gmail.com>
2025-11-12 01:18:03 +0530

    filters added in alertStore.js

--- frontend/src/store/alertStore.js
+++ frontend/src/store/alertStore.js
@@ -1,19 +1,54 @@
 import { create } from "zustand";
 import { fetchAlerts, fetchAbnormal } from "../api/alertsApi";
 
-export const useAlertStore = create((set) => ({
+export const useAlertStore = create((set, get) => ({
   alerts: [],
+  filteredAlerts: [],
   abnormal: [],
-  loading: false,
+  filters: {
+    cameraId: "",
+    label: "",
+    severity: ""
+  },
 
   loadAlerts: async () => {
-    set({ loading: true });
     const data = await fetchAlerts();
-    set({ alerts: data, loading: false });
+    set({ alerts: data });
+    get().applyFilters();
   },
 
   loadAbnormal: async () => {
     const data = await fetchAbnormal();
     set({ abnormal: data });
   },
+
+  setFilter: (key, value) => {
+    set(state => ({
+      filters: { ...state.filters, [key]: value }
+    }));
+    get().applyFilters();
+  },
+
+  resetFilters: () => {
+    set({
+      filters: { cameraId: "", label: "", severity: "" }
+    });
+    get().applyFilters();
+  },
+
+  applyFilters: () => {
+    const { alerts, filters } = get();
+
+    const rows = alerts.filter(a => {
+      if (filters.cameraId && a.event.cameraId !== filters.cameraId)
+        return false;
+      if (filters.label && a.event.label !== filters.label)
+        return false;
+      if (filters.severity && a.severity !== filters.severity)
+        return false;
+      return true;
+    });
+
+    set({ filteredAlerts: rows });
+  }
 }));

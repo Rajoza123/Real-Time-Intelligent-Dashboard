db56c8c106e231c8593831f2a45ae91a7d23827d
Rajoza123 <ozaraj76@gmail.com>
2025-11-11 18:47:29 +0530

    updated alert fetching limit

--- frontend/src/api/alertsApi.js
+++ frontend/src/api/alertsApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE = "https://real-time-intelligent-dashboard.onrender.com/api";
 
 export const fetchAlerts = async () => {
-  const res = await axios.get(`${BASE}/alerts?limit=100`);
+  const res = await axios.get(`${BASE}/alerts?limit=50`);
   return res.data.alerts;
 };
 
